2798f15d6e4e88206f547cb12cb42751e37f678f
DEVENDRA SAHU <133871247+devendrasahu0@users.noreply.github.com>
2023-10-25 14:47:33 +0530

    Add files via upload

---
+++ PerfectSquare.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class PerfectSquare {
+    // Function to check if a number is a perfect square
+    public static boolean isPerfectSquare(int num) {
+        int sqrt = (int) Math.sqrt(num);
+        return sqrt * sqrt == num;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = sc.nextInt();
+
+        if (isPerfectSquare(number)) {
+            System.out.println(number + " is a perfect square.");
+        } else {
+            System.out.println(number + " is not a perfect square.");
+        }
+    }
+}
+
